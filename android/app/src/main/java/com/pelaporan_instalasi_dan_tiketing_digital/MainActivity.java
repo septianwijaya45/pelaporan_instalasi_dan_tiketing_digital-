c570a805fa1deef791beb2d357e7de3f38246ff8
septianwijaya45 <septianwijaya45@gmail.com>
2022-03-16 20:25:17 +0700

    first commit

---
+++ android/app/src/main/java/com/pelaporan_instalasi_dan_tiketing_digital/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pelaporan_instalasi_dan_tiketing_digital;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "pelaporan_instalasi_dan_tiketing_digital";
+  }
+}
