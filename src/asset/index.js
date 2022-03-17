03b6df08b94d8d323feb501bf7acbbae5d4f3f49
septianwijaya45 <septianwijaya45@gmail.com>
2022-03-17 09:28:10 +0700

    move logo to folder asset/logo

--- src/asset/index.js
+++ src/asset/index.js
@@ -1,6 +1,19 @@
 import bgImage from './images/bgTransparent.png';
 import userIcon from './icons/user.png';
 import passIcon from './icons/padlock.png';
-import logo1Icon from './icons/logo1.png';
+import logo1Icon from './logo/logo1.png';
+import logo2Icon from './logo/logo1.png';
+import logo3Icon from './logo/logo1.png';
+import logo4Icon from './logo/logo1.png';
+import logo5Icon from './logo/logo1.png';
 
-export {bgImage, userIcon, passIcon, logo1Icon};
+export {
+  bgImage,
+  userIcon,
+  passIcon,
+  logo1Icon,
+  logo2Icon,
+  logo3Icon,
+  logo4Icon,
+  logo5Icon,
+};
